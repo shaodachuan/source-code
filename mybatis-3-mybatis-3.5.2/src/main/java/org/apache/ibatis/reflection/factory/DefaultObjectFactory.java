5a6d7522e8fcf1ae78f8157765f879da050ac9b0
red2222 <466483135@qq.com>
2023-12-31 22:00:47 +0800

    mybatis的reflection包 - ObjectFactory - 根据Class，创建实例 - DefaultObjectFactory - 根据Class，参数、参数类型，使用反射，创建实例

---
+++ mybatis-3-mybatis-3.5.2/src/main/java/org/apache/ibatis/reflection/factory/DefaultObjectFactory.java
@@ -0,0 +1,136 @@
+/**
+ *    Copyright 2009-2019 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.apache.ibatis.reflection.factory;
+
+import java.io.Serializable;
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+import org.apache.ibatis.reflection.ReflectionException;
+import org.apache.ibatis.reflection.Reflector;
+
+/**
+ * @author Clinton Begin
+ */
+public class DefaultObjectFactory implements ObjectFactory, Serializable {
+
+  private static final long serialVersionUID = -8855120656740914948L;
+
+  @Override
+  public <T> T create(Class<T> type) {
+    return create(type, null, null);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public <T> T create(Class<T> type, List<Class<?>> constructorArgTypes, List<Object> constructorArgs) {
+    Class<?> classToCreate = resolveInterface(type);
+    // we know types are assignable
+    return (T) instantiateClass(classToCreate, constructorArgTypes, constructorArgs);
+  }
+
+  /**
+   * 创建类的实例
+   * @param type  类型
+   * @param constructorArgTypes 参数类型列表
+   * @param constructorArgs 参数类型
+   * @return  实例
+   * @param <T> 实例类型
+   */
+  private  <T> T instantiateClass(Class<T> type, List<Class<?>> constructorArgTypes, List<Object> constructorArgs) {
+    try {
+      // 需要调用的构造方法
+      Constructor<T> constructor;
+
+      // 如果参数类型列表 == null
+      // constructor赋值为无参构造, 出昂见实例
+      if (constructorArgTypes == null || constructorArgs == null) {
+        constructor = type.getDeclaredConstructor();
+        try {
+          return constructor.newInstance();
+        } catch (IllegalAccessException e) {
+          // 发生异常，设置constructor访问性，重试
+          if (Reflector.canControlMemberAccessible()) {
+            constructor.setAccessible(true);
+            return constructor.newInstance();
+          } else {
+            throw e;
+          }
+        }
+      }
+
+      // 根据参数列表，获取constuctor，创建实例
+      constructor = type.getDeclaredConstructor(constructorArgTypes.toArray(new Class[constructorArgTypes.size()]));
+      try {
+        return constructor.newInstance(constructorArgs.toArray(new Object[constructorArgs.size()]));
+      } catch (IllegalAccessException e) {
+        if (Reflector.canControlMemberAccessible()) {
+          constructor.setAccessible(true);
+          return constructor.newInstance(constructorArgs.toArray(new Object[constructorArgs.size()]));
+        } else {
+          throw e;
+        }
+      }
+    } catch (Exception e) {
+      // 创建失败
+      // 收集所有参数信息，抛出一个异常
+      String argTypes = Optional.ofNullable(constructorArgTypes).orElseGet(Collections::emptyList)
+          .stream().map(Class::getSimpleName).collect(Collectors.joining(","));
+      String argValues = Optional.ofNullable(constructorArgs).orElseGet(Collections::emptyList)
+          .stream().map(String::valueOf).collect(Collectors.joining(","));
+      throw new ReflectionException("Error instantiating " + type + " with invalid types (" + argTypes + ") or values (" + argValues + "). Cause: " + e, e);
+    }
+  }
+
+  /**
+   * 根据接口Class，返回实现类的Class
+   * @param type 接口的Class
+   * @return  实现类的Class
+   */
+  protected Class<?> resolveInterface(Class<?> type) {
+    Class<?> classToCreate;
+    // List Collection Iterable  -> 返回ArrayList
+    if (type == List.class || type == Collection.class || type == Iterable.class) {
+      classToCreate = ArrayList.class;
+    } else if (type == Map.class) {
+      classToCreate = HashMap.class;
+    } else if (type == SortedSet.class) { // issue #510 Collections Support
+      classToCreate = TreeSet.class;
+    } else if (type == Set.class) {
+      classToCreate = HashSet.class;
+    } else {
+      classToCreate = type;
+    }
+    return classToCreate;
+  }
+
+  @Override
+  public <T> boolean isCollection(Class<T> type) {
+    return Collection.class.isAssignableFrom(type);
+  }
+
+}
